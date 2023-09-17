b4f7a0f3fb419c655fee516db7e034a4de31ff01
George Higbie <gehigbie@gmail.com>
2023-09-17 14:34:41 -0500

    Add db connection config

---
+++ src/db/mongoose.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/twitter-api", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, // Add this option for Mongoose v6 and above
+});
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("./db/mongoose");
 const User = require("./models/user");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,6 +14,15 @@ app.get("/users", async (req, res) => {
     res.status(200).send(users);
   } catch (err) {
     res.status(400).send(err);
+  }
+});
+
+app.post("/users", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+  } catch (err) {
+    res.status(500).send(err);
   }
 });
 
